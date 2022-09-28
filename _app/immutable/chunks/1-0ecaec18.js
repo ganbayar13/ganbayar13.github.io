4a14aea93e732d2b78148fcf281d87cce4c13e36
ganbayar13 <ganbayar13@gmail.com>
2022-09-28 14:38:36 +0800

    Updates

---
+++ _app/immutable/chunks/1-0ecaec18.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-817fec7f.js";export{t as component};
--- _app/immutable/chunks/1-ac8d4369.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-eda526b2.js";export{t as component};
