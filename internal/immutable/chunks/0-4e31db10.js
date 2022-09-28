1dac901373b0a72fbba4459634d710a438f8f8ad
ganbayar13 <ganbayar13@gmail.com>
2022-09-28 14:45:05 +0800

    Updates

---
+++ internal/immutable/chunks/0-4e31db10.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_layout.svelte-c7c9f9fe.js";export{t as component};
--- internal/immutable/chunks/0-ac60c8e0.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_layout.svelte-bb8d287e.js";export{t as component};
