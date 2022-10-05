ce4f8f9acbbd36a52e89f99f56a7a9939e64f6f0
ganbayar13 <ganbayar13@gmail.com>
2022-10-05 18:37:21 +0800

    Update button.svelte

--- src/lib/button.svelte
+++ src/lib/button.svelte
@@ -14,14 +14,14 @@
 		href={link}
 		target="_blank"
 		class:w-full={full}
-		class="{className} group text-white/80 hover:text-white/100 inline-flex focus:ring-4 focus:outline-none font-medium rounded-lg py-2.5 px-4 transition duration-300 ease-in-out overflow-hidden"
+		class="{className} group text-white/80 hover:text-white/100 inline-flex focus:ring-4 focus:outline-none font-medium rounded-lg py-3 px-4 transition duration-300 ease-in-out overflow-hidden"
 	>
-		<span class="icon sm:group-hover:scale-[4] sm:group-hover:-rotate-45 sm:group-hover:opacity-20 sm:transition">
+		<div class="icon sm:group-hover:scale-[4] sm:group-hover:-rotate-45 sm:group-hover:opacity-20 sm:transition">
 			<slot name="icon" />
-		</span>
-		<span class="border-r border-white/10 mx-4 -my-1 sm:group-hover:opacity-0 sm:transition-opacity" />
-		<span class="text sm:group-hover:-translate-x-4 sm:group-hover:scale-125  sm:transition-transform">
+		</div>
+		<div class="border-r border-white/10 mx-4 -my-1.5 sm:group-hover:opacity-0 sm:transition-opacity" />
+		<div class="text sm:group-hover:-translate-x-4 sm:group-hover:scale-125  sm:transition-transform">
 			<slot name="text" />
-		</span>
+		</div>
 	</a>
 {/if}
