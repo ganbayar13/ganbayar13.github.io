27f13437397b945ff8c7bac0c9702d7ae1a77e3d
ganbayar13 <ganbayar13@gmail.com>
2022-10-05 18:27:10 +0800

    updates

--- src/lib/button.svelte
+++ src/lib/button.svelte
@@ -14,10 +14,14 @@
 		href={link}
 		target="_blank"
 		class:w-full={full}
-		class="{className} block focus:ring-4 focus:outline-none font-medium rounded-lg px-5 py-2.5 transition duration-300 ease-in-out"
+		class="{className} group text-white/70 hover:text-white/100 inline-flex focus:ring-4 focus:outline-none font-medium rounded-lg py-2.5 px-4 transition duration-300 ease-in-out overflow-hidden"
 	>
-		<span class="inline-flex items-center text-white">
-			<slot />
+		<span class="icon group-hover:scale-150 group-hover:rotate-6 transition">
+			<slot name="icon" />
+		</span>
+		<span class="border-r border-white/10 mx-4 -my-1 group-hover:opacity-0 transition-opacity" />
+		<span class="text group-hover:-translate-x-4 transition-transform">
+			<slot name="text" />
 		</span>
 	</a>
 {/if}
