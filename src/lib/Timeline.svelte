96f90eb00ccd82f581dd147a3aef41ef77de57d0
ganbayar13 <ganbayar13@gmail.com>
2023-02-01 10:04:26 +0800

    fixed

--- src/lib/Timeline.svelte
+++ src/lib/Timeline.svelte
@@ -84,7 +84,7 @@
 				<a
 					target="_blank"
 					href={item.website}
-					class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700 mt-4"
+					class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 mt-4"
 					><svg
 						xmlns="http://www.w3.org/2000/svg"
 						viewBox="0 0 20 20"
