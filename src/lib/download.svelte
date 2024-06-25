96c9a04d9169c7a4d7aecf8d88e07635aa163a72
ganbayar13 <ganbayar13@gmail.com>
2024-06-25 16:50:11 +0800

    Update download.svelte

--- src/lib/download.svelte
+++ src/lib/download.svelte
@@ -11,8 +11,7 @@
 
 <button
 	type="button"
-	class="bg-white px-4 py-2 w-full rounded-full shadow-lg uppercase text-black font-bold truncate"
-	on:click={download}
+	class="bg-white opacity-25 cursor-default px-4 py-2 w-full rounded-full shadow-lg uppercase text-black font-bold truncate"
 >
 	<span>Download Resume</span>
 </button>
