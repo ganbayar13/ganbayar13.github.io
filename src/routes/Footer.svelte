5d4637652a62df5df1f599bec45e51fe868eec8e
ganbayar13 <ganbayar13@gmail.com>
2025-06-27 04:27:02 +0900

    Update Footer.svelte

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -4,23 +4,19 @@
 	year = now.getFullYear();
 </script>
 
-<footer class="footer  text-sm">
-	<div class="">
-		<div class="mt-24">
-			<div>
-				Built with <a href="https://svelte.dev/" target="_blank" class="font-bold underline"
-					>Svelte</a
-				>,
-				<a href="https://tailwindcss.com/" target="_blank" class="font-bold underline"
-					>Tailwind CSS</a
-				> and more...
-			</div>
-			<div>
-				Hosted on <a href="https://pages.github.com/" target="_blank" class="font-bold underline"
-					>Github Pages</a
-				>.
-			</div>
-			<div class="sm:order-first">Copyright ©{year}. Made with <span title="love">❤️</span>.</div>
+<footer class="footer text-sm mt-24 text-gray-500">
+	<div>
+		<div>
+			Built with <a href="https://svelte.dev/" target="_blank" class="font-bold underline">Svelte</a
+			>,
+			<a href="https://tailwindcss.com/" target="_blank" class="font-bold underline">Tailwind CSS</a
+			> and more...
 		</div>
+		<div>
+			Hosted on <a href="https://pages.github.com/" target="_blank" class="font-bold underline"
+				>Github Pages</a
+			>.
+		</div>
+		<div class="sm:order-first">Copyright ©{year}. Made with <span title="love">❤️</span>.</div>
 	</div>
 </footer>
