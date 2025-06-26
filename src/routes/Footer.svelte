d0a0caf7058612b0104c3997421caa44eec6cdd7
ganbayar13 <ganbayar13@gmail.com>
2025-06-27 04:22:21 +0900

    added footer

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -4,21 +4,23 @@
 	year = now.getFullYear();
 </script>
 
-<footer class="footer bg-black text-white p-5 text-sm">
-	<div class="lg:container lg:mx-auto">
-		<div class="sm:flex sm:justify-between">
+<footer class="footer  text-sm">
+	<div class="">
+		<div class="mt-24">
 			<div>
 				Built with <a href="https://svelte.dev/" target="_blank" class="font-bold underline"
 					>Svelte</a
 				>,
 				<a href="https://tailwindcss.com/" target="_blank" class="font-bold underline"
 					>Tailwind CSS</a
-				>,
-				<a href="https://pages.github.com/" target="_blank" class="font-bold underline"
-					>Github Pages</a
 				> and more...
 			</div>
-			<div class="sm:order-first">Copyright © {year}. Made with <span title="love">❤️</span></div>
+			<div>
+				Hosted on <a href="https://pages.github.com/" target="_blank" class="font-bold underline"
+					>Github Pages</a
+				>.
+			</div>
+			<div class="sm:order-first">Copyright ©{year}. Made with <span title="love">❤️</span>.</div>
 		</div>
 	</div>
 </footer>
